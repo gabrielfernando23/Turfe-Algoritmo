8ca0e05c236d298565aeba992e287a7540643490
NataSFSilva <nata.ssilva@sptech.school>
2022-12-01 14:30:00 -0300

    tentativa de merge

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
     var cavalos = []
-
+    var qntdVoltas =0;
 function cadastrarCavalo() {
     if (cavalos.length < 3) {
         cavalos.push(ipt_nomeCavalo.value)
@@ -14,18 +14,76 @@ function cadastrarCavalo() {
    }
 }
 
+// function cadastrarVoltas() {
+//     qntdVoltas = Number (ipt_qntdVoltasCavalo.value);
+//     qntdVoltas++;
+//     exibir.innerHTML += `sua corrida terá ${qntdVoltas}voltas`
+// }
 function Corrida() {
 
-    
-    for (let index = 0; index < 7; index++) {
+    let contador = 0;
+   var velocidadeTotalCavalo1 = 0;
+   var velocidadeTotalCavalo2 = 0;
+   var velocidadeTotalCavalo3 = 0;
+//    var velocidadeTotalCavalo4 = 0;
+//    var velocidadeTotalCavalo5 = 0;
+//    var velocidadeTotalCavalo6 = 0;
+//    var velocidadeTotalCavalo7 = 0;
+   
+
+    for (let index = 0; index < qntdVoltas ; index++) {
         var velocidade_cavalo1 = Math.random() *3 +7;
         var velocidade_cavalo2 = Math.random() *3 +7;
         var velocidade_cavalo3 = Math.random() *3 +7;
-        cavalos.push({volta: `volta${index+1}`,cavalo1Velocidade:`${velocidade_cavalo1}`,
-        cavalo2Velocidade:`${velocidade_cavalo2}`,
-        cavalo3Velocidade:`${velocidade_cavalo3}`})
+        // var velocidade_cavalo4 = Math.random() *3 +7;
+        // var velocidade_cavalo5 = Math.random() *3 +7;
+        // var velocidade_cavalo6 = Math.random() *3 +7;
+        // var velocidade_cavalo7 = Math.random() *3 +7;
+
+        velocidadeTotalCavalo1+=velocidade_cavalo1;
+        velocidadeTotalCavalo2+=velocidade_cavalo2;
+        velocidadeTotalCavalo3+=velocidade_cavalo3;
+        // velocidadeTotalCavalo4+=velocidade_cavalo4;
+        // velocidadeTotalCavalo5+=velocidade_cavalo5;
+        // velocidadeTotalCavalo6+=velocidade_cavalo6;
+        // velocidadeTotalCavalo7+=velocidade_cavalo7;
+
+        cavalos.push({volta: `volta${index+1}`,cavalo1VelocidadeTotal:`${velocidade_cavalo1}`,
+        cavalo1VelocidadeTotal:`${velocidadeTotalCavalo1}`,
+        cavalo2VelocidadeTotal:`${velocidade_cavalo2}`,
+        cavalo2VelocidadeTotal:`${velocidadeTotalCavalo2}`,
+        cavalo3VelocidadeTotal:`${velocidade_cavalo3}`,
+        cavalo3VelocidadeTotal:`${velocidadeTotalCavalo3}`});
+        // cavalo4VelocidadeTotal:`${velocidade_cavalo4}`,
+        // cavalo4VelocidadeTotal:`${velocidadeTotalCavalo4}`,
+        // cavalo5VelocidadeTotal:`${velocidade_cavalo5}`,
+        // cavalo5VelocidadeTotal:`${velocidadeTotalCavalo5}`,
+        // cavalo6VelocidadeTotal:`${velocidade_cavalo6}`,
+        // cavalo6VelocidadeTotal:`${velocidadeTotalCavalo6}`,
+        // cavalo7VelocidadeTotal:`${velocidade_cavalo7}`,
+        // cavalo7VelocidadeTotal:`${velocidadeTotalCavalo7}`,})
         console.log(cavalos)
+        largada.innerHTML+=``
+        contador++;    
     }
+
+    if (cavalo1VelocidadeTotal > cavalo2VelocidadeTotal && cavalo1VelocidadeTotal > cavalo3VelocidadeTotal){
+        if (cavalo2VelocidadeTotal > cavalo3VelocidadeTotal) {
+            
+        } else {
+            
+        }
+    }
+    if (cavalo2VelocidadeTotal > cavalo3VelocidadeTotal && cavalo2VelocidadeTotal > cavalo1VelocidadeTotal){
+        
+    }
+    if (cavalo3VelocidadeTotal > cavalo1VelocidadeTotal && cavalo3VelocidadeTotal > cavalo2VelocidadeTotal){
+        
+    }
+
+
+
+  
 }
 
 function segundaPagina() {
