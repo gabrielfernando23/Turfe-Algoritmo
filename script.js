25229659a73a7c5904df50857dcfa29ffbae3781
gabrielfernando23 <gabriel.ssilva@sptech.school>
2022-12-01 15:59:07 -0300

    att

--- script.js
+++ script.js
@@ -17,38 +17,45 @@ function Corrida() {
    var velocidadeTotalCavalo3 = 0;
 
     for (let index = 0; index < 7; index++) {
-        var velocidade_cavalo1 = Math.random() *3 +7;
-        var velocidade_cavalo2 = Math.random() *3 +7;
-        var velocidade_cavalo3 = Math.random() *3 +7;
-        velocidadeTotalCavalo1+=velocidade_cavalo1;
-        velocidadeTotalCavalo2+=velocidade_cavalo2;
-        velocidadeTotalCavalo3+=velocidade_cavalo3;
+        var velocidade_cavalo1 = (Math.random() *3 +7).toFixed(2)
+        var velocidade_cavalo2 = (Math.random() *3 +7).toFixed(2)
+        var velocidade_cavalo3 = (Math.random() *3 +7).toFixed(2)
+        velocidadeTotalCavalo1+= parseFloat(velocidade_cavalo1)
+        velocidadeTotalCavalo2+= parseFloat(velocidade_cavalo2)
+        velocidadeTotalCavalo3+= parseFloat(velocidade_cavalo3)
 
         cavalos.push({volta:`volta${index+1}`,cavalo1Velocidade:`${velocidade_cavalo1}`,
-        cavalo1VelocidadeTotal:`${velocidadeTotalCavalo1}`,
+        cavalo1VelocidadeTotal:`${(velocidadeTotalCavalo1).toFixed(2)}`,
         cavalo2Velocidade:`${velocidade_cavalo2}`,
-        cavalo2VelocidadeTotal:`${velocidadeTotalCavalo2}`,
+        cavalo2VelocidadeTotal:`${(velocidadeTotalCavalo2).toFixed(2)}`,
         cavalo3Velocidade:`${velocidade_cavalo3}`,
-        cavalo3VelocidadeTotal:`${velocidadeTotalCavalo3}`});
+        cavalo3VelocidadeTotal:`${(velocidadeTotalCavalo3).toFixed(2)}`,})
         console.log(cavalos)
-        largada.innerHTML+=``
-        index++;    
-        largada.innerHTML += `${cavalos[index].volta} - Cavalo 1 -${cavalos[index].cavalo1Velocidade} - ${cavalos[index].cavalo1VelocidadeTotal}- Cavalo 2 - ${cavalos[index].cavalo2Velocidade} - ${cavalos[index].cavalo2VelocidadeTotal} - Cavalo 3 - ${cavalos[index].cavalo3Velocidade} - ${cavalos[index].cavalo3VelocidadeTotal}`
+        largada.innerHTML+=`` 
+        largada.innerHTML += `<br>${cavalos[index].volta} - Cavalo 1 -${cavalos[index].cavalo1Velocidade} - ${(cavalos[index].cavalo1VelocidadeTotal)}- Cavalo 2 - ${cavalos[index].cavalo2Velocidade} - ${(cavalos[index].cavalo2VelocidadeTotal)} - Cavalo 3 - ${cavalos[index].cavalo3Velocidade} - ${(cavalos[index].cavalo3VelocidadeTotal)}`
     }
 
     if (cavalo1VelocidadeTotal > cavalo2VelocidadeTotal && cavalo1VelocidadeTotal > cavalo3VelocidadeTotal){
-       largada.innerHTML += `1`
+       largada.innerHTML += `Campeão : Cavalo 1`
         if (cavalo2VelocidadeTotal > cavalo3VelocidadeTotal) {
-            
+            largada.innerHTML += ``
         } else {
             
         }
     }
     if (cavalo2VelocidadeTotal > cavalo3VelocidadeTotal && cavalo2VelocidadeTotal > cavalo1VelocidadeTotal){
-        
+        if (cavalo1VelocidadeTotal > cavalo3VelocidadeTotal) {
+            
+        } else {
+            
+        }
     }
     if (cavalo3VelocidadeTotal > cavalo1VelocidadeTotal && cavalo3VelocidadeTotal > cavalo2VelocidadeTotal){
-        
+        if (cavalo1VelocidadeTotal > cavalo2Velocidade) {
+            
+        } else {
+            
+        }
     }
 
 
